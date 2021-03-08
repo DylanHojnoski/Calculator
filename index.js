57487af962f2c988c4425bf40038e7f975d054ec
DylanHojnoski <dylanhoj@gmail.com>
2021-03-08 13:03:09 -0500

    Added calculator functionality

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let num1;
 let num2;
 let operator;
 
+const screen = document.getElementById("screen");
+const body = document.querySelector("body");
+body.addEventListener("load", assignButtons());
 function add (num1, num2)
 {
     return num1 + num2;
@@ -34,22 +37,97 @@ function operate()
     switch(operator)
     {
         case "+":
-            add(num1,num2);
+            screen.textContent = add(num1,num2);
             break;
 
         case "-":
-            subtract(num1, num2);
+            screen.textContent = subtract(num1, num2);
             break;
 
         case "*":
-            multiply(num1, num2);
+            screen.textContent = multiply(num1, num2);
             break;
 
         case "/": 
-            divide(num1, num1);
+            screen.textContent = divide(num1, num1);
             break;
         
         default:
             break;
     }
+}
+
+function assignButtons()
+{
+    const buttons = document.querySelectorAll("button");
+    let regex = /\d/;
+    for(let  i = 0; i < buttons.length; i++)
+    {
+        buttons[i].addEventListener("click", () => {
+            if (regex.test(buttons[i].textContent))
+            {
+                assignNum(buttons[i].textContent);
+                console.log("press");
+            }
+            else if (buttons[i].textContent == "C")
+            {
+                clear();
+            }
+            else if(buttons[i].textContent != "=")
+            {
+                assignOperator(buttons[i].textContent);
+                console.log("ress1");
+            }
+            else
+            {
+                num1 = Number(num1);
+                num2 = Number(num2);
+                operate();
+               // clear();
+            }
+        });
+    }
+}
+
+function assignNum(num)
+{
+    if(operator == undefined)
+    {
+        if(num1 == undefined)
+        {
+            num1 = num;
+        }
+        else
+        {
+            num1 = Number(num1 * 10) + Number(num);
+        }
+        screen.textContent = num1;
+    }
+    else
+    {
+        if(num2 == undefined)
+        {
+            num2 = num;
+        }
+        else
+        {
+        num2 = Number(num2 * 10) + Number(num);
+        }
+        screen.textContent = num2;
+    }
+    return num;
+}
+
+function assignOperator(op)
+{
+    operator = op;
+    screen.textContent = op;
+}
+
+function clear()
+{
+    screen.textContent = "";
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
 }
